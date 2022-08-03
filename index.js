fb5245749a90a887acf164df9a9fa7cca6aa4c00
Eric Song <ericksong91@gmail.com>
2022-08-03 19:27:58 -0400

    done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+const batteryReducer = (accu, init) => accu + init;
+const totalBatteries = batteryBatches.reduce(batteryReducer, 0)
+
+console.log(totalBatteries)
